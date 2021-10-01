62666778377f9b03febf3bd2e64f522278bf53b1
Mucyosoda <mucyosofa@gmail.com>
2021-10-01 19:07:37 +0200

    add localstorage

---
+++ src/check.js
@@ -0,0 +1,123 @@
+/* eslint no-use-before-define: ["error", { "variables": false }] */
+
+function updateCompletedListArray() {
+  completedTasks = [];
+
+  taskList.forEach((task) => {
+    if (task.done) completedTasks.push(`${taskList.indexOf(task)}`);
+  });
+}
+
+function saveLocalList() {
+  localStorage.setItem('taskList', JSON.stringify(taskList));
+}
+
+function toggleChecked(e) {
+  const checkStatus = e.target.checked;
+  const task = e.target.parentElement;
+  const taskId = task.id;
+  let removed = false;
+
+  taskList[taskId].done = checkStatus;
+
+  if (completedTasks.length === 0) {
+    completedTasks.push(taskId);
+  } else {
+    completedTasks.forEach((index) => {
+      if (taskId === index) {
+        completedTasks.splice(completedTasks.indexOf(index), 1);
+        removed = true;
+      }
+    });
+
+    if (!removed) {
+      completedTasks.push(taskId);
+      completedTasks.sort();
+    }
+  }
+
+  saveLocalList();
+}
+
+function updateListView() {
+  const ul = document.getElementById('taskList');
+
+  ul.innerHTML = '';
+
+  taskList.forEach((task) => {
+    const listItem = document.createElement('li');
+    const taskLabel = document.createElement('label');
+    const delBtn = document.createElement('span');
+    const dotBtn = document.createElement('span');
+    const checkbox = document.createElement('input');
+
+    listItem.className = 'task';
+    listItem.id = taskList.indexOf(task);
+
+    taskLabel.className = 'taskLabel';
+    taskLabel.textContent = task.name;
+    taskLabel.htmlFor = `c${taskList.indexOf(task)}`;
+
+    delBtn.className = 'deleteTaskBtn';
+    delBtn.innerHTML = '<i class="fas fa-trash"></i>';
+
+    dotBtn.className = 'dotbtn';
+    dotBtn.innerHTML = '<i class="fas fa-ellipsis-v"></i>';
+    dotBtn.onclick = '';
+
+    checkbox.className = 'taskCheckbox';
+    checkbox.id = `c${taskList.indexOf(task)}`;
+    checkbox.type = 'checkbox';
+    checkbox.checked = task.done;
+    checkbox.onclick = toggleChecked;
+
+    listItem.appendChild(checkbox);
+    listItem.appendChild(taskLabel);
+    listItem.appendChild(dotBtn);
+    listItem.appendChild(delBtn);
+    ul.appendChild(listItem);
+  });
+}
+
+function checkDuplicate(task) {
+  let matchFound = false;
+
+  taskList.forEach((t) => {
+    if (t.name === task) matchFound = true;
+  });
+
+  return matchFound;
+}
+
+function addToList(task) {
+  if (checkDuplicate(task)) {
+    return;
+  }
+
+  taskList.push({
+    name: task,
+    done: false,
+  });
+
+  updateListView();
+
+  localStorage.setItem('taskList', JSON.stringify(taskList));
+  document.querySelector('#taskInput').value = '';
+}
+
+document.querySelector('#taskInput').addEventListener('keypress', function (e) {
+  const key = e.which || e.keyCode;
+  if (key === 13 && document.querySelector('#taskInput').value.length > 0) {
+    addToList(this.value.trim());
+  }
+});
+
+let taskList = [];
+let completedTasks = [];
+
+if (JSON.parse(localStorage.getItem('taskList'))) {
+  taskList = JSON.parse(localStorage.getItem('taskList'));
+} else localStorage.setItem('taskList', JSON.stringify(taskList));
+
+updateCompletedListArray();
+updateListView();
--- src/index.js
+++ src/index.js
@@ -1,35 +1,59 @@
 // import _ from 'lodash';
 
 import './style.css';
+import activeCheckbox from './check.js';
 
-const listTable = [
-  {
-    description: 'go to the market',
-    completed: true,
-    index: 0,
-  },
-  {
-    description: 'wash dishes',
-    completed: true,
-    index: 1,
-  },
-  {
-    description: 'play guitar',
-    completed: true,
-    index: 2,
-  },
-  'rwanda',
-];
+window.onload = function () {
+  // const addTodos = document.querySelector('.todo-button');
+  // const textTodo = document.querySelector('.todo-input');
+  const todoList = document.querySelector('.todo-list');
 
-const todoList = document.querySelector('.todo-list');
-// eslint-disable-no-use-before-define
-function displayTodo(item) {
-  return ` <div class='todo'>
+  const listTable = [
+    {
+      description: 'go to the market',
+      completed: false,
+      index: 0,
+    },
+    {
+      description: 'wash dishes',
+      completed: false,
+      index: 1,
+    },
+    {
+      description: 'Complete my project',
+      completed: false,
+      index: 2,
+    },
+    {
+      description: 'do sport',
+      completed: false,
+      index: 3,
+    },
+  ];
+
+  let dayTodo = [];
+  const localStoregeContent = localStorage.getItem('todo');
+  if (localStoregeContent === null) {
+    dayTodo = listTable;
+  } else {
+    dayTodo = JSON.parse(localStoregeContent);
+  }
+
+  localStorage.setItem('todo', JSON.stringify(dayTodo));
+  console.log(dayTodo);
+  for (let i = 0; i < listTable.length; i += 1) {
+    listTable[i] = dayTodo[i];
+  }
+
+  function displayTodo(item) {
+    return ` <div class='todo'>
   <div class="checkbox"><input type="checkbox"></div>
   <div class="description">${item.description}</div>
   <div><i class="fas fa-ellipsis-v"></i></div>
   </div>`;
-}
+  }
 
-document.addEventListener('DOMContentLoaded', displayTodo);
-todoList.innerHTML = `${listTable.map(displayTodo).join('')}`;
+  document.addEventListener('DOMContentLoaded', displayTodo);
+  todoList.innerHTML = `${dayTodo.map(displayTodo).join('')}`;
+  todoList.addEventListener('click', activeCheckbox);
+};
